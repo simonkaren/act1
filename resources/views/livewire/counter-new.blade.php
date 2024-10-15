03da7650e6c0db41da749ae864c753573c0b841c
Simon Karen <simonkaren21@gmail.com>
2024-10-15 13:29:21 +0800

    branch ADP changes

--- resources/views/livewire/counter-new.blade.php
+++ resources/views/livewire/counter-new.blade.php
@@ -6,4 +6,5 @@
         <button wire:click="increment" type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
             Increment</button>
 </div>
+<h1>Updated to ADP branch</h1>
 </div>
