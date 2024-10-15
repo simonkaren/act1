91ab538011d7a00a43e0f84085644a97faabcf29
Simon Karen <simonkaren21@gmail.com>
2024-10-15 13:18:42 +0800

    add counter

---
+++ resources/views/livewire/counter-new.blade.php
@@ -0,0 +1,9 @@
+<div class="flex p-10 items-center flex-col">
+    <h1 class="text-3xl mb-2">{{$number}}</h1>
+    <div>
+        <button wire:click="decrement" type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-red-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+            Decrement</button>
+        <button wire:click="increment" type="button" class="focus:outline-none text-white bg-purple-700 hover:bg-green-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">
+            Increment</button>
+</div>
+</div>
